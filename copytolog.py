f97bb262de73d1727a598f1b103242acef7419b6
Petteri Pesonen <petteri.pesonen@karttakeskus.fi>
2017-09-24 10:03:08 +0300

    updated .gitingore and removed unnecessary files

--- copytolog.py
+++
@@ -1,2 +0,0 @@
-# TODO make the current json to be copied to the arkisto folder as new file -> copy and transfer
-# TODO log script should also modify the file name as such: yleisviesti_<timestamp>.json
