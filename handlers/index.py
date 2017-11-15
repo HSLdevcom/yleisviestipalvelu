5fa5a688a4c4c0480ee5cf6eedb68f045168cba0
Tuomas Starck <dev@starck.fi>
2017-11-15 21:37:21 +0200

    Make pylint and pycodestyle a bit more happy
    All changes should be only cosmetic and not functional. Also, pylint
    will still complain about missing docstrings.

--- handlers/index.py
+++ handlers/index.py
@@ -1,19 +1,20 @@
-import tornado.web
 import json
 import shutil
 import time
-import settings
 
+from tornado import web
 from tornado.options import options
 
-class ViewJson(tornado.web.RequestHandler):
+
+class ViewJson(web.RequestHandler):
     def get(self):
         with open(options.filename) as data_file:
             data = json.load(data_file)
 
         self.write(json.dumps(data, indent=4, ensure_ascii=False))
 
-class ViewEdit(tornado.web.RequestHandler):
+
+class ViewEdit(web.RequestHandler):
     def get(self):
         with open(options.filename) as data_file:
             data = json.load(data_file)
@@ -24,13 +25,15 @@ class ViewEdit(tornado.web.RequestHandler):
             json=json.dumps(data)
         )
 
-class CopyJson(tornado.web.RequestHandler):
+
+class CopyJson(web.RequestHandler):
     def post(self):
         # retrieve the message as json
         data = json.loads(self.request.body.decode('utf-8'))
 
         # first copy the file to archive
-        shutil.copy2(options.filename, options.filepath + 'yleisviesti_' + time.strftime("%d%m%Y_%H%M%S") + '.json')
+        shutil.copy2(options.filename, options.filepath + 'yleisviesti_' +
+                     time.strftime("%d%m%Y_%H%M%S") + '.json')
 
         # open existing file and overwrite it with new json
         with open(options.filename, 'w') as outfile:
--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
-from tornado.options import define, options
+from tornado.options import define
 
 define("port", default=8900, help="run on the given port", type=int)
-define("filename", default="yleisviesti-latest.json", help="the served file", type=str)
 define("filepath", default="archive/", help="path for backup files", type=str)
+define("filename", default="yleisviesti-latest.json", help="the served file",
+       type=str)
