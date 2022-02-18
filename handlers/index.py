b3184f449e61dc69f80dfd71be32a35f8fe7d76e
Vesa Meskanen <vesa.meskanen@cgi.com>
2022-02-18 08:59:13 +0200

    fix: safety problem fixes from Jussi Viertola

--- handlers/index.py
+++ handlers/index.py
@@ -1,6 +1,7 @@
 import json
 import shutil
 import time
+import re
 
 from tornado import web
 from tornado.options import options
@@ -10,20 +11,34 @@ class ViewJson(web.RequestHandler):
     def get(self):
         with open(options.filename) as data_file:
             data = json.load(data_file)
-
-        self.write(json.dumps(data, indent=4, ensure_ascii=False))
-
+        self.write(
+            re.sub(
+                u'[^a-zA-Z0-9åäöÅÄÖ^_.,:;!#%&/*(){}[=?`´\'@£$€~| \-\]\"\\\n]+',
+                '',
+                json.dumps(
+                    data,
+                    indent=4,
+                    ensure_ascii=False
+                )
+            )
+        )
+        self.set_header('Content-type', 'application/json')
 
 class ViewEdit(web.RequestHandler):
     def get(self):
         with open("templates/edit.html",'rb') as tpl_file:
             self.write(tpl_file.read())
 
-
 class CopyJson(web.RequestHandler):
     def post(self):
         # retrieve the message as json
-        data = json.loads(self.request.body.decode('utf-8'))
+        data = json.loads(
+            re.sub(
+                u'[^a-zA-Z0-9åäöÅÄÖ^_.,:;!#%&/*(){}[=?`´\'@£$€~| \-\]\"\\\n]+',
+                '',
+                self.request.body.decode('utf-8')
+            )
+        )
 
         # first copy the file to archive
         shutil.copy2(options.filename, options.filepath + 'yleisviesti_' +
